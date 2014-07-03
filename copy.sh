8d6b79482930620cf2a1dc1381777298470d5227
AndiH <a.herten+github@gmail.com>
2014-07-03 16:56:35 +0200

    Finetuning

--- copy.sh
+++ copy.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 main() {
 	local copyToPath="/Users/Andi/Documents/Studium/Promotion/Thesis/LaTeX"
-	for file in {_packages.tex,_settings.tex,_newcommands.tex,_titlepage.tex,.latexmkrc,template.sublime-project}; do
+	for file in {_packages.tex,_settings.tex,_newcommands.tex,.latexmkrc,.gitignore,template.sublime-project}; do
 		cp $file $copyToPath
 	done
+	mv $copyToPath/template.sublime-project $copyToPath/thesis.sublime-project
 }
 main
